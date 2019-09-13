ff263ab3e4ea9a9d5ce30e3f41fd50d70a689dbd
Jean-Baptiste Martin <jb@expend.io>
2019-09-13 03:09:22 +0100

    init from Antoine

---
+++ precache-manifest.93208790c1a2bacaf71d3587639cb3f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37153da226961b34042fffe9978b14a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af7d97e9c95c4661aff4",
+    "url": "/static/css/main.5e1f44d2.chunk.css"
+  },
+  {
+    "revision": "e2ed888c93894424157b",
+    "url": "/static/js/2.12b9965a.chunk.js"
+  },
+  {
+    "revision": "af7d97e9c95c4661aff4",
+    "url": "/static/js/main.e98dc524.chunk.js"
+  },
+  {
+    "revision": "9b442d402ed558d4ee99",
+    "url": "/static/js/runtime~main.f051e85b.js"
+  },
+  {
+    "revision": "6d17269ab0932b34f8fc37cd3ef3b302",
+    "url": "/static/media/bucket.6d17269a.mp4"
+  },
+  {
+    "revision": "213cf499578a4fa6c09a6524c013a950",
+    "url": "/static/media/jeffoufe.213cf499.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a05087f55a6a0f95a55fd4be82415896",
+    "url": "/static/media/mix.a05087f5.png"
+  },
+  {
+    "revision": "aa0abe509c01b62e4851457062a232dc",
+    "url": "/static/media/stardust.aa0abe50.mp3"
+  },
+  {
+    "revision": "9eeec9bd35460aa0d3d8ea2fef570c79",
+    "url": "/static/media/what.9eeec9bd.png"
+  }
+]);
